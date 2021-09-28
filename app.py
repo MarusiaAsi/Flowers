216693a405723b0b51daf4f0dd9e16b6f85be66a
MarusiaTitova <marina.marmelad777@gmail.com>
2021-09-28 09:37:40 +0300

    venv

--- app.py
+++ app.py
@@ -17,6 +17,30 @@ def makecalc():
 
 
 if __name__ == '__main__':
-    app.run(port=5000, host='127.0.0.1')
+    app.run(port=8080, host='127.0.0.1')
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(debug=True, host='0.0.0.0')
+
+# import os
+#
+# from flask import Flask, request, jsonify
+# import pickle as p
+#
+# app = Flask(__name__)
+#
+# model = p.load(open('knnpickle_file', 'rb'))
+#
+#
+# @app.route('/', methods=['POST'])
+# def makecalc() :
+#     data = request.get_json()
+#     prediction = model.predict(data)
+#     flower = ['Setosa', 'Versicolor', 'Virginica']
+#     prediction = flower[int(prediction)]
+#     return jsonify(prediction)
+#
+#
+# if __name__ == '__main__' :
+#     app.run(port=8080, host='127.0.0.1')
     # port = int(os.environ.get("PORT", 5000))
     # app.run(debug=True, host='0.0.0.0')
